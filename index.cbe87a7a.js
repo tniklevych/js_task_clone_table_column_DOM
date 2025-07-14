dad3ad8d635079d6bb6dbccd2a719244db4b98bc
Taras Niklevych <niklevychtaras@gmail.com>
2025-07-14 10:54:18 +0300

    make build

---
+++ index.cbe87a7a.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("table tr").forEach(function(e){var r=e.children;if(!(r.length<2)){var l=r[1].cloneNode(!0);e.insertBefore(l,r[r.length-1])}});
+//# sourceMappingURL=index.cbe87a7a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const rows = document.querySelectorAll("table tr");
+rows.forEach((row)=>{
+    const cells = row.children;
+    if (cells.length < 2) return;
+    const secondCell = cells[1];
+    const clone = secondCell.cloneNode(true);
+    row.insertBefore(clone, cells[cells.length - 1]);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
