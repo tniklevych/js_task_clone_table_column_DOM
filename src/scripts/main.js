f8eef9e02d34650b1c2bf8b16f897bb56e237850
Taras Niklevych <niklevychtaras@gmail.com>
2025-07-14 10:53:51 +0300

    add cells length check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,11 @@ const rows = document.querySelectorAll('table tr');
 
 rows.forEach((row) => {
   const cells = row.children;
+
+  if (cells.length < 2) {
+    return;
+  }
+
   const secondCell = cells[1];
   const clone = secondCell.cloneNode(true);
 
